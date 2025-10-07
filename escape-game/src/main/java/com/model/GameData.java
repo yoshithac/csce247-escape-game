177d6100a758091a1e81aea6fa64f02981fb3d3b
CSE-AashrithA <Sunnypandu64@gmail.com>
2025-10-07 14:25:57 -0400

    Fully implemented GameData class

--- escape-game/src/main/java/com/model/GameData.java
+++ escape-game/src/main/java/com/model/GameData.java
@@ -12,6 +12,54 @@ public class GameData {
     private List<Hint> hints;
     private List<Certificate> certificates;
     
+    /**
+     * Returns the list of puzzles in the game
+     * @return List of puzzles
+     */
+    public List<Puzzle> getPuzzles() {
+        return puzzles;
+    }
+
+    /**
+     * Sets the list of puzzles in the game
+     * @param puzzles Puzzle list of puzzles
+     */
+    public void setPuzzles(List<Puzzle> puzzles) {
+        this.puzzles = puzzles;
+    }
+
+    /**
+     * Returns the current game progress
+     * @return GameProgress 
+     */
+    public GameProgress getGameProgress() {
+        return gameProgress;
+    }
+
+    /**
+     * Sets the game progress
+     * @param gameProgress GameProgress
+     */
+    public void setGameProgress(GameProgress gameProgress) {
+        this.gameProgress = gameProgress;
+    }
+
+    /**
+     * Returns the leaderboard of the game
+     * @return Leaderboard object
+     */
+    public Leaderboard getLeaderboard() {
+        return leaderboard;
+    }
+
+    /**
+     * Sets the leaderboard for thegame
+     * @param leaderboard Leaderboard 
+     */
+    public void setLeaderboard(Leaderboard leaderboard) {
+        this.leaderboard = leaderboard;
+    }
+
     /*
      * Returns the list of hints
      * @return List of hints applicable to the game
@@ -20,6 +68,27 @@ public class GameData {
         return hints;
     }
 
+    /**
+     * Sets the list of hints for the game
+     * @param hints List of hints
+     */
+    public void setHints(List<Hint> hints) {
+        this.hints = hints;
+    }
 
+    /**
+     * Returns the certificates earned in the game
+     * @return List of certificates
+     */
+    public List<Certificate> getCertificates() {
+        return certificates;
+    }
 
+    /**
+     * Sets the list of certificates for the game
+     * @param certificates List of certificates
+     */
+    public void setCertificate(List<Certificate> certificates) {
+        this.certificates = certificates;
+    }
 }
