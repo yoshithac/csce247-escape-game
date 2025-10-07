8b88fba9f6273560066f1f493d6c316bfc305ffe
yoshithac <yoshichilukuri@gmail.com>
2025-10-07 13:45:23 -0400

    removed testing doc

--- escape-game/src/main/java/com/model/GameData.java
+++ escape-game/src/main/java/com/model/GameData.java
@@ -1,7 +1,7 @@
 package com.model;
 import java.util.List;
 
-/*
+/**
  * This class holds the game data for the escape game application
  * @author We're Getting an A
  */
@@ -12,14 +12,13 @@ public class GameData {
     private List<Hint> hints;
     private List<Certificate> certificates;
     
-    /*
+    /**
      * Returns the list of hints
-     * @return list of hints applicable to the game
+     * @return List of hints
      */
     public List<Hint> getHints() {
         return hints;
     }
-
 
 
 }
