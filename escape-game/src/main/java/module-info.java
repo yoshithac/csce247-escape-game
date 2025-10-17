36c15f181b0335bcc36d3c5e3d09c7e3eba95fa8
yoshithac <yoshichilukuri@gmail.com>
2025-10-17 14:36:56 -0400

    fixed pom file; and module-info

--- escape-game/src/main/java/module-info.java
+++ escape-game/src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.escapegame {
-    requires javafx.controls;
+requires javafx.controls;
     requires javafx.fxml;
     requires json.simple;
-    requires java.desktop;
+    requires com.google.gson;
 
     opens com.escapegame to javafx.fxml;
     exports com.escapegame;
