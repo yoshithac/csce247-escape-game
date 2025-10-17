9999e6c29e6e55cabd1b91f75549035e510d6e82
yoshithac <yoshichilukuri@gmail.com>
2025-10-17 15:13:54 -0400

    added gamedata.json implementation to gamedataloader

--- escape-game/src/main/java/com/model/GameDataLoader.java
+++ escape-game/src/main/java/com/model/GameDataLoader.java
@@ -1,21 +1,18 @@
 package com.model;
 
-import java.io.File;
 import java.io.FileReader;
 import java.util.List;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import com.google.gson.GsonBuilder;
 
 public class GameDataLoader {
 	
 	protected static final String USER_FILE_NAME = "escape-game/src/main/resources/Users.json";
-
-
+    protected static final String GAMEDATA_FILE_NAME = "escape-game/src/main/resources/GameData.json";
 
     /**
-	 * Loads the list of system usernames and permissions.
+	 * Loads the list of user data.
 	 * @param usersFile
 	 * @throws Exception
 	 */
@@ -41,5 +38,33 @@ public class GameDataLoader {
         throw new Exception("File not found: Users file not found", ex);
     }
     return users;
+}
+/**
+ * Loads list of all game data (i.e. Leaderboeards, puzzles, etc)
+ * @return gameDataFile
+ * @throws Exception
+ */
+public List<GameData> loadGameData() throws Exception {
+String gameDataFile = GAMEDATA_FILE_NAME;
+
+List<GameData> gameData;
+try {
+        gameData = new Gson().fromJson(
+            new FileReader(gameDataFile),
+            new TypeToken<List<GameData>>() {}.getType()
+        );
+        
+        // Check if gameData is null
+        if (gameData == null) {
+            System.out.println("Warning: JSON file is empty or returned null");
+            gameData = new java.util.ArrayList<>(); // Return empty list instead of null
+        }
+        
+    } catch (Exception ex) {
+        System.out.println("Could not find: " + gameDataFile);
+        ex.printStackTrace();
+        throw new Exception("File not found: Users file not found", ex);
+    }
+    return gameData;
 }
 }
