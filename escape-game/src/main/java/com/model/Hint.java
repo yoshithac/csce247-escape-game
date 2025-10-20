7f2a3d29e3009775d23f70d7db1f27120aa9e081
nitinrlr <ramaling@sc.edu>
2025-10-19 21:24:19 -0400

    added comments to hint class

--- escape-game/src/main/java/com/model/Hint.java
+++ escape-game/src/main/java/com/model/Hint.java
@@ -1,7 +1,9 @@
 package com.model;
 
-/*
- * This class represents a hint that can be given to players
+/**
+ * Represents a hint that can be given to players in the escape game.
+ * Manages how many hints a player can use based on the difficulty level
+ * 
  * @author We're Getting an A
  */
 public class Hint extends GameManager {
@@ -10,6 +12,12 @@ public class Hint extends GameManager {
     private String puzzleId;
     private String hintText;
 
+    /**
+     * Constructor for Hint
+     * @param puzzleId the ID of the puzzle this hint belongs to
+     * @param hintText the text of the hint to display
+     * @param maxHints the maximum number of hints allowed
+     */
     public Hint(String puzzleId, String hintText, int maxHints) {
         this.puzzleId = puzzleId;
         this.hintText = hintText;
@@ -17,6 +25,12 @@ public class Hint extends GameManager {
         this.hintsUsed = 0;
     }
 
+    /**
+     * Provides a hint for the specified puzzle if available.
+     * Increments the number of hints used each time a hint is provided.
+     * @param puzzleId the ID of the puzzle for which a hint is requested
+     * @return the hint text if available, or a message indicating no more hints can be used
+     */
     public String provideHint(String puzzleId) {
         if (canUseHint()) {
             hintsUsed++;
@@ -25,14 +39,25 @@ public class Hint extends GameManager {
         return "No more hints available.";
     }
 
+    /**
+     * Checks whether player can still use a hint.
+     * @return true if hints remaining are available, false otherwise
+     */
     public boolean canUseHint() {
         return hintsUsed < maxHints;
     }
 
+    /**
+     * Resets the number of hints used back to zero.
+     */
     public void resetHints() {
         hintsUsed = 0;
     }
 
+    /**
+     * Sets the maximum number of hints based on the game's difficulty level.
+     * @param maxHints the default maximum number of hints (ignored, as difficulty determines actual value)
+     */
     public void setMaxHints(int maxHints) {
         if (getDifficulty() == DifficultyLevel.EASY) {
             this.maxHints = 5;
@@ -43,26 +68,50 @@ public class Hint extends GameManager {
         }
     }
 
+    /**
+     * Getter for puzzleId
+     * @return puzzleId the ID of the puzzle this hint is for
+     */
     public String getPuzzleId() {
         return puzzleId;
     }
 
+    /**
+     * Setter for puzzleId
+     * @param puzzleId the ID of the puzzle this hint is for
+     */
     public void setPuzzleId(String puzzleId) {
         this.puzzleId = puzzleId;
     }
 
+    /**
+     * Getter for hintsUsed
+     * @return hintsUsed the number of hints already used
+     */
     public int getHintsUsed() {
         return hintsUsed;
     }
 
+    /**
+     * Setter for hintsUsed
+     * @param hintsUsed the number of hints that have been used
+     */
     public void setHintsUsed(int hintsUsed) {
         this.hintsUsed = hintsUsed;
     }
 
+    /**
+     * Getter for hintText
+     * @return hintText the text of the hint
+     */
     public String getHintText() {
         return hintText;
     }
 
+    /**
+     * Setter for hintText
+     * @param hintText the text of the hint
+     */
     public void setHintText(String hintText) {
         this.hintText = hintText;
     }
