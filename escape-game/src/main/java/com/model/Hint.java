7bd7849afea54bea3b3b6bfde8bf6a4e0652a530
nitinrlr <ramaling@sc.edu>
2025-10-26 16:17:50 -0400

    changes

--- escape-game/src/main/java/com/model/Hint.java
+++ escape-game/src/main/java/com/model/Hint.java
@@ -1,118 +1,54 @@
 package com.model;
 
 /**
- * Represents a hint that can be given to players in the escape game.
- * Manages how many hints a player can use based on the difficulty level
- * 
- * @author We're Getting an A
+ * Hint entity for puzzle hints
+ * Hints are ordered by priority (1 = first, 2 = second, etc.)
  */
-public class Hint extends GameManager {
-    private int hintsUsed;
-    private int maxHints;
-    private String puzzleId;
+public class Hint implements Comparable<Hint> {
     private String hintText;
-
-    /**
-     * Constructor for Hint
-     * @param puzzleId the ID of the puzzle this hint belongs to
-     * @param hintText the text of the hint to display
-     * @param maxHints the maximum number of hints allowed
-     */
-    public Hint(String puzzleId, String hintText, int maxHints) {
-        this.puzzleId = puzzleId;
+    private String puzzleId;
+    private int hintPriority;
+    
+    public Hint() {}
+    
+    public Hint(String hintText, String puzzleId, int hintPriority) {
         this.hintText = hintText;
-        this.maxHints = maxHints;
-        this.hintsUsed = 0;
-    }
-
-    /**
-     * Provides a hint for the specified puzzle if available.
-     * Increments the number of hints used each time a hint is provided.
-     * @param puzzleId the ID of the puzzle for which a hint is requested
-     * @return the hint text if available, or a message indicating no more hints can be used
-     */
-    public String provideHint(String puzzleId) {
-        if (canUseHint()) {
-            hintsUsed++;
-            return hintText;
-        }
-        return "No more hints available.";
-    }
-
-    /**
-     * Checks whether player can still use a hint.
-     * @return true if hints remaining are available, false otherwise
-     */
-    public boolean canUseHint() {
-        return hintsUsed < maxHints;
-    }
-
-    /**
-     * Resets the number of hints used back to zero.
-     */
-    public void resetHints() {
-        hintsUsed = 0;
-    }
-
-    /**
-     * Sets the maximum number of hints based on the game's difficulty level.
-     * @param maxHints the default maximum number of hints (ignored, as difficulty determines actual value)
-     */
-    public void setMaxHints(int maxHints) {
-        if (getDifficulty() == DifficultyLevel.EASY) {
-            this.maxHints = 5;
-        } else if (getDifficulty() == DifficultyLevel.MEDIUM) {
-            this.maxHints = 3;
-        } else if (getDifficulty() == DifficultyLevel.HARD) {
-            this.maxHints = 1;
-        }
-    }
-
-    /**
-     * Getter for puzzleId
-     * @return puzzleId the ID of the puzzle this hint is for
-     */
-    public String getPuzzleId() {
-        return puzzleId;
-    }
-
-    /**
-     * Setter for puzzleId
-     * @param puzzleId the ID of the puzzle this hint is for
-     */
-    public void setPuzzleId(String puzzleId) {
         this.puzzleId = puzzleId;
+        this.hintPriority = hintPriority;
     }
-
-    /**
-     * Getter for hintsUsed
-     * @return hintsUsed the number of hints already used
-     */
-    public int getHintsUsed() {
-        return hintsUsed;
+    
+    // Getters and Setters
+    public String getHintText() { 
+        return hintText; 
     }
-
-    /**
-     * Setter for hintsUsed
-     * @param hintsUsed the number of hints that have been used
-     */
-    public void setHintsUsed(int hintsUsed) {
-        this.hintsUsed = hintsUsed;
+    
+    public void setHintText(String hintText) { 
+        this.hintText = hintText; 
     }
-
-    /**
-     * Getter for hintText
-     * @return hintText the text of the hint
-     */
-    public String getHintText() {
-        return hintText;
+    
+    public String getPuzzleId() { 
+        return puzzleId; 
     }
-
-    /**
-     * Setter for hintText
-     * @param hintText the text of the hint
-     */
-    public void setHintText(String hintText) {
-        this.hintText = hintText;
+    
+    public void setPuzzleId(String puzzleId) { 
+        this.puzzleId = puzzleId; 
+    }
+    
+    public int getHintPriority() { 
+        return hintPriority; 
+    }
+    
+    public void setHintPriority(int hintPriority) { 
+        this.hintPriority = hintPriority; 
+    }
+    
+    @Override
+    public int compareTo(Hint other) {
+        return Integer.compare(this.hintPriority, other.hintPriority);
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("[Priority %d] %s", hintPriority, hintText);
     }
 }
